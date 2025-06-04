93bca03d7f81db388be45d540ee182af720da791
Hisham-Tariq <Hishamtariq.hi@gmail.com>
2025-06-04 14:11:33 +0500

    verison updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="elasticutils",
-    version="0.1.0",
+    version="0.2.0",
     description="A simple Elasticsearch Utilities library",
     author="Cybersilo",
     packages=find_packages(),
